97bc5632536aa9998b7c90b882ae25408435858d
meneguzzoS <meneguzzo_stefano@hotmail.it>
2023-03-23 23:52:18 +0100

    struttura area personale

--- Trustify/src/app/app.module.ts
+++ Trustify/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AreaPersonaleComponent } from './area-personale/area-personale.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AreaPersonaleComponent
   ],
   imports: [
     BrowserModule,
