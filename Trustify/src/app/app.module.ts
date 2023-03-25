90dd9484e4c633b97a4f95fa697685f5af7b4bc6
Marco <Rosin>
2023-03-25 10:06:50 +0100

    corretto path componente

--- Trustify/src/app/app.module.ts
+++ Trustify/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PagamentoComponent } from './pagamento/pagamento/pagamento.component';
+import { PagamentoComponent } from './pagamento/pagamento.component';
 
 @NgModule({
   declarations: [
