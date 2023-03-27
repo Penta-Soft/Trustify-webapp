b8cc1cb1d6bd6f456f5efba09175e8e30479ed09
Marco <Rosin>
2023-03-27 16:32:25 +0200

    aggiunto routing pagina pagamento

--- Trustify/src/app/app-routing.module.ts
+++ Trustify/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PagamentoComponent } from './pagamento/pagamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'pagamento', component: PagamentoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
