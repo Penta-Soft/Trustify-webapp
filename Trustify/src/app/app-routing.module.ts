2daf61b5b424059f1c54ad1ae4d10667cc5d6b05
MarcatoLuca <luca.marcato.2@studenti.unipd.it>
2023-03-28 15:40:09 +0200

    Rimosso routing dell applicazione

--- Trustify/src/app/app-routing.module.ts
+++ Trustify/src/app/app-routing.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AreaPersonaleComponent } from './area-personale/area-personale.component';
-
 import { HomePageComponent } from './home-page/home-page.component';
-
 import { PagamentoComponent } from './pagamento/pagamento.component';
 
 const routes: Routes = [
-  { path: 'pagamento', component: PagamentoComponent },
-  { path: 'areapersonale', component: AreaPersonaleComponent },
-  { path: '', component: HomePageComponent },
+  // { path: 'pagamento', component: PagamentoComponent },
+  // { path: 'areapersonale', component: AreaPersonaleComponent },
+  // { path: '', component: HomePageComponent },
 ];
 
 @NgModule({
