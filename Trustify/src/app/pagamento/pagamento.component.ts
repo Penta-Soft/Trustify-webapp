da9c34a40d4cfe72072b1b8cb7b6d81383ecf4b5
Marco <Rosin>
2023-04-04 11:56:51 +0200

    completato form pagamento material

--- Trustify/src/app/pagamento/pagamento.component.ts
+++ Trustify/src/app/pagamento/pagamento.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-pagamento',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./pagamento.component.css']
 })
 export class PagamentoComponent {
+  form: FormGroup = new FormGroup({});
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      address: [null, [Validators.required, Validators.pattern("^0x[a-fA-F0-9]{40}$")]],
+      tokens: [null, [Validators.required, Validators.pattern("(?<!\d)0*[1-9]\d*")]]
+    });
+  }
+  
+  onSubmit(form: any): void {
+    alert("HEH, VOLEVI!");
+  }
 }
