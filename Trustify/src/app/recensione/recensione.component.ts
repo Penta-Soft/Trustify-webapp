38e4ee414599b0d5636146507015b5056d9ce06f
meneguzzoS <meneguzzo_stefano@hotmail.it>
2023-04-13 20:06:54 +0200

    aggiunto tasto annulla per annullare la modifica

--- Trustify/src/app/recensione/recensione.component.ts
+++ Trustify/src/app/recensione/recensione.component.ts
@@ -11,7 +11,6 @@ export class RecensioneComponent implements OnInit {
   arrayRatings: number[] = [];
   arrayAddresses: string[] = [];
   len: number[] = [];
-  // starArr: number[] = [0,1,2,3,4];
   modified: boolean = false;
   rating: number = 0;
   starCount: number = 5;
@@ -47,17 +46,20 @@ export class RecensioneComponent implements OnInit {
 
   async editReview(index: number) {
     let btn = document.getElementById('modifica'+index);
+    let btn2 = document.getElementById('cancella'+index);
     let recensione = document.getElementById('descrizione'+index);
 
     if(!this.modified && btn?.innerText == 'Modifica') {
       if(recensione) recensione.setAttribute('contenteditable', 'true');
       if(btn) btn.innerHTML = 'Fatto';
+      if(btn2) btn2.innerHTML = 'Annulla';
       this.rating = 0;
       this.modified = true;
     } else if (this.modified && btn?.innerText == 'Fatto') {
       //chiamata a writeAReview()
       if(recensione) recensione.setAttribute('contenteditable', 'false');
       if(btn) btn.innerHTML = 'Modifica';
+      if(btn2) btn2.innerHTML = 'Cancella';
       let recensioneText = recensione?.innerText;
       console.log(recensione?.innerText);
       let indirizzo = document.getElementById('indirizzo'+index)?.innerText;
@@ -65,26 +67,25 @@ export class RecensioneComponent implements OnInit {
       this.modified = false;
       console.log('prova'+this.arrayRatings[index]);
       if(this.rating==0) this.rating = this.arrayRatings[index];
-      if(indirizzo)
-      this.web3.WriteAReview(indirizzo,String(recensioneText),this.rating);
+      if(indirizzo) this.web3.WriteAReview(indirizzo,String(recensioneText),this.rating);
     } else {
       alert('Finisci la modifica');
     }
   }
 
-  async removeReview(index: number) {
-
-  }
-
-  async deleteReview(address: string) {
-    await this.web3.DeleteReview(address);
+  async deleteReview(index: number) {
+    // await this.web3.DeleteReview(this.arrayAddresses[index]);
+    let btn2 = document.getElementById('cancella'+index);
+    if(btn2?.innerText == 'Annulla') {
+      let btn = document.getElementById('modifica'+index);
+      if(btn) btn.innerHTML = 'Modifica';
+      this.modified = false;
+      btn2.innerHTML = 'Cancella';
+    }
   }
 
   onRatingChanged(rating: number) {
     if(this.modified) {
-      // console.log(rating);
-      // this.arrayRatings[value] = rating;
-      // console.log('prova1'+this.arrayRatings[value]);
         this.rating = rating;
         console.log('prova1'+this.rating);
     }
