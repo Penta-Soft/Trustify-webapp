a35b477db8ff93e4c8afd680b8029ec184b87af4
lazza <nicola.lazzarin89@gmail.com>
2023-04-13 11:32:27 +0200

    added function delete review

--- Trustify/src/app/recensione/recensione.component.ts
+++ Trustify/src/app/recensione/recensione.component.ts
@@ -4,37 +4,40 @@ import { Web3Service } from '../web3.service';
 @Component({
   selector: 'app-recensione',
   templateUrl: './recensione.component.html',
-  styleUrls: ['./recensione.component.css']
+  styleUrls: ['./recensione.component.css'],
 })
-export class RecensioneComponent implements OnInit{
+export class RecensioneComponent implements OnInit {
   arrayDescriptions: string[] = [];
   arrayRatings: number[] = [];
   arrayAddresses: string[] = [];
   len: number[] = [];
-  starArr: number[] = [0,1,2,3,4];
+  starArr: number[] = [0, 1, 2, 3, 4];
   modified: boolean = false;
   // @Output() private ratingUpdated = new EventEmitter();//aggiuntoa
-
 
   constructor(private web3: Web3Service) {}
 
   ngOnInit(): void {
-    const reviews = this.web3.GetNMyReview(0,10);
-    reviews.then((val) => {
-      this.arrayDescriptions = val[0];
-      // console.log('arrayDescriptions'+this.arrayDescriptions);
-      this.arrayRatings = val[1];
-      this.arrayAddresses = val[2];
-      console.log('dim'+val[2].length);
-      for(let i = 0; i < val[2].length; i++) {
-        this.len.push(i);
-      }
-    }).catch((val) => {'problem'});
+    const reviews = this.web3.GetNMyReview(0, 10);
+    reviews
+      .then((val) => {
+        this.arrayDescriptions = val[0];
+        // console.log('arrayDescriptions'+this.arrayDescriptions);
+        this.arrayRatings = val[1];
+        this.arrayAddresses = val[2];
+        console.log('dim' + val[2].length);
+        for (let i = 0; i < val[2].length; i++) {
+          this.len.push(i);
+        }
+      })
+      .catch((val) => {
+        'problem';
+      });
   }
 
   showStarsReview(index: number, status: number) {
     // console.log('i'+index+'s'+status);
-    if(index < this.arrayRatings[status]) {
+    if (index < this.arrayRatings[status]) {
       return 'star';
     } else {
       return 'star_border';
@@ -44,17 +47,17 @@ export class RecensioneComponent implements OnInit{
   async editReview(index: number) {
     // let n:number = await this.web3.GetNumberReviews('0x46280DAc7ddB65FD98cFEd1C53D848068b5ccb3E');
     // console.log('number'+n);
-    let btn = document.getElementById('modifica'+index);
-    let descrizione = document.getElementById('descrizione'+index);
+    let btn = document.getElementById('modifica' + index);
+    let descrizione = document.getElementById('descrizione' + index);
 
-    if(!this.modified && btn?.innerText == 'Modifica') {
-      if(descrizione) descrizione.setAttribute('contenteditable', 'true');
-      if(btn) btn.innerHTML = 'Fatto';
+    if (!this.modified && btn?.innerText == 'Modifica') {
+      if (descrizione) descrizione.setAttribute('contenteditable', 'true');
+      if (btn) btn.innerHTML = 'Fatto';
       this.modified = true;
-    } else if(this.modified && btn?.innerText == 'Fatto') {
+    } else if (this.modified && btn?.innerText == 'Fatto') {
       //chiamata a writeAReview()
-      if(descrizione) descrizione.setAttribute('contenteditable', 'false');
-      if(btn) btn.innerHTML = 'Modifica';
+      if (descrizione) descrizione.setAttribute('contenteditable', 'false');
+      if (btn) btn.innerHTML = 'Modifica';
       // console.log(descrizione?.innerText);
       this.modified = false;
     } else {
@@ -62,10 +65,13 @@ export class RecensioneComponent implements OnInit{
     }
   }
 
+  async deleteReview(address: string) {
+    await this.web3.DeleteReview(address);
+  }
+
   // onClick(rating:number) {
   //   console.log(rating)
   //   this.ratingUpdated.emit(rating);
   //   return false;
   // }
-
 }
