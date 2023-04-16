c0f6851ee9f4c880b00960091bc567c32dfec116
Luca Marcato <marcatoluca15@gmail.com>
2023-04-16 15:46:04 +0200

    fix login state and tab index on refresh. close #31

--- Trustify/src/app/header/header.component.ts
+++ Trustify/src/app/header/header.component.ts
@@ -1,17 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-  firstPage: number = 0;
+export class HeaderComponent implements OnInit {
   isMetamaskConnected: boolean = false;
+  currentIndex: number = 0;
+
+  ngOnInit() {
+    if (localStorage.getItem("login") === "true") {
+      this.isMetamaskConnected = true;
+    }
+    let index = localStorage.getItem("index");
+    this.currentIndex = index !== null ? parseInt(index) : 0;
+  }
+
+  updateCurrentIndex(index: number): void {
+    this.currentIndex = index;
+    localStorage.setItem("index", index.toString());
+  }
 
   changeMetamaskState(): void {
     this.isMetamaskConnected = !this.isMetamaskConnected;
-    this.firstPage = 0;
+
+    if (this.isMetamaskConnected) {
+      localStorage.setItem("login", "true");
+    } else {
+      localStorage.setItem("login", "false");
+    }
+
+    this.currentIndex = 0;
   }
 
 }
