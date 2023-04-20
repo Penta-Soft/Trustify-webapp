76257e8a705100d6fd4692ae712daa6eeb0eeed3
Luca Marcato <marcatoluca15@gmail.com>
2023-04-20 11:01:10 +0200

    aggiunto servizio di parsing delle recensioni in area personale

--- Trustify/src/app/area-personale/area-personale.component.ts
+++ Trustify/src/app/area-personale/area-personale.component.ts
@@ -1,42 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { Web3Service } from '../web3.service';
-
+import { RecensioniParserService } from '../recensioni-parser.service';
+import { Recensione } from '../recensione';
 @Component({
   selector: 'app-area-personale',
   templateUrl: './area-personale.component.html',
   styleUrls: ['./area-personale.component.css'],
 })
 export class AreaPersonaleComponent implements OnInit {
-  arrayReviews: string[] = [];
-  arrayRatings: number[] = [];
-  arrayAddresses: string[] = [];
-  arrayStatus: string[] = [];
-  len: number[] = [];
-  EditInProgress: boolean = false;
 
-  constructor(private web3: Web3Service) { }
+  reviews: Recensione[] = [];
 
-  ngOnInit(): void {
-    const reviews = this.web3.GetNMyReview(0, 10);
-    reviews
-      .then((val) => {
-        this.arrayReviews = val[0];
-        this.arrayRatings = val[1];
-        this.arrayAddresses = val[3];
-        console.log('dim' + val[2].length);
-        for (let i = 0; i < val[2].length; i++) {
-          this.len.push(i);
-        }
-        this.arrayStatus = val[2];
-      })
-      .catch((val) => {
-        'problem';
-      });
+  private readonly REVIEWS_FROM = 0;
+  private readonly REVIEWS_TO = 10;
+
+  constructor(private reviewParserService: RecensioniParserService) { }
+
+  async ngOnInit() {
+    this.reviews = await this.reviewParserService.retrivePersonalAreaReviews(this.REVIEWS_FROM, this.REVIEWS_TO);
   }
-
-  check(modifica: boolean) {
-    this.EditInProgress = modifica;
-    console.log('ap' + this.EditInProgress)
-  }
-
 }
