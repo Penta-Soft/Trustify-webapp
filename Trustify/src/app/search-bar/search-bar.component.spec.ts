6fc5f1a7bd38839890c6c73ac3936254f9391b97
Luca Marcato <marcatoluca15@gmail.com>
2023-05-01 12:25:31 +0200

    test base 'should create' di search bar component passato

--- Trustify/src/app/search-bar/search-bar.component.spec.ts
+++ Trustify/src/app/search-bar/search-bar.component.spec.ts
@@ -1,16 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { SearchBarComponent } from './search-bar.component';
+import { RecensioniParserService } from '../recensioni-parser.service';
 
 describe('SearchBarComponent', () => {
   let component: SearchBarComponent;
   let fixture: ComponentFixture<SearchBarComponent>;
 
   beforeEach(async () => {
+
+    let reviewParserServiceSpy = jasmine.createSpyObj('RecensioniParserService', ['retriveHomePageReviews'])
+
     await TestBed.configureTestingModule({
-      declarations: [ SearchBarComponent ]
+      declarations: [SearchBarComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+      providers: [
+        { provide: RecensioniParserService, useValue: reviewParserServiceSpy }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(SearchBarComponent);
     component = fixture.componentInstance;
