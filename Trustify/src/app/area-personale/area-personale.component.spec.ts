fc5eab0c8911a0d325455876874c0d55abd09256
Luca Marcato <marcatoluca15@gmail.com>
2023-05-01 12:22:53 +0200

    test base 'should create' di area personale passato

--- Trustify/src/app/area-personale/area-personale.component.spec.ts
+++ Trustify/src/app/area-personale/area-personale.component.spec.ts
@@ -1,16 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AreaPersonaleComponent } from './area-personale.component';
+import { RecensioniParserService } from '../recensioni-parser.service';
 
 describe('AreaPersonaleComponent', () => {
   let component: AreaPersonaleComponent;
   let fixture: ComponentFixture<AreaPersonaleComponent>;
 
   beforeEach(async () => {
+    let reviewParserServiceSpy = jasmine.createSpyObj('RecensioniParserService', ['retrivePersonalAreaReviews'])
     await TestBed.configureTestingModule({
-      declarations: [ AreaPersonaleComponent ]
+      declarations: [AreaPersonaleComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [{ provide: RecensioniParserService, useValue: reviewParserServiceSpy }]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(AreaPersonaleComponent);
     component = fixture.componentInstance;
