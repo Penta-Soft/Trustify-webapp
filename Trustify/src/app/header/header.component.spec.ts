2f50bc6c99169f5fb41be6db31ab793be3682ccd
Luca Marcato <marcatoluca15@gmail.com>
2023-05-01 12:23:36 +0200

    test base 'should create' di header component passato

--- Trustify/src/app/header/header.component.spec.ts
+++ Trustify/src/app/header/header.component.spec.ts
@@ -1,16 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HeaderComponent } from './header.component';
+import { WalletService } from '../wallet.service';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
   beforeEach(async () => {
+
+    let walletServiceSpy = jasmine.createSpyObj('WalletService', ['connect'])
+
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [HeaderComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [{ provide: WalletService, useValue: walletServiceSpy }]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
