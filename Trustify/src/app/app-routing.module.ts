cda4f20b20007f4b36e14928f9e34a3f25d7dfdf
Luca Marcato <marcatoluca15@gmail.com>
2023-05-01 12:20:59 +0200

    rimossi import non utilizzati

--- Trustify/src/app/app-routing.module.ts
+++ Trustify/src/app/app-routing.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AreaPersonaleComponent } from './area-personale/area-personale.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { PagamentoComponent } from './pagamento/pagamento.component';
 
 const routes: Routes = [];
 
