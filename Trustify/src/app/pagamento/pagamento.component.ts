673ccd20b855a4c147010dea3dca5076ad696a53
lazza <nicola.lazzarin89@gmail.com>
2023-05-05 11:46:27 +0200

    aggiunta snack-bar alla pagina pagamento

--- Trustify/src/app/pagamento/pagamento.component.ts
+++ Trustify/src/app/pagamento/pagamento.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
-import {
-  FormControl,
-  FormGroup,
-  FormBuilder,
-  Validators,
-} from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Web3Service } from '../web3.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-pagamento',
@@ -16,22 +12,48 @@ export class PagamentoComponent {
   form: FormGroup = new FormGroup({});
   accountBalance: number = 0;
   isProgressSpinnerVisible: boolean = false;
-  constructor(private formBuilder: FormBuilder, private web3: Web3Service) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private web3: Web3Service,
+    private snackBar: MatSnackBar
+  ) {
     this.getTokenBalance();
   }
 
   async pay(address: string, amount: string) {
     this.isProgressSpinnerVisible = true;
-    this.web3.approveTokens(parseInt(amount)).finally(() => {
-      this.web3.depositTokens(address, parseInt(amount)).finally(() => {
+    await this.web3.approveTokens(parseInt(amount)).finally(async () => {
+      try {
+        await this.web3.depositTokens(address, parseInt(amount)).finally(() => {
+          this.isProgressSpinnerVisible = false;
+          this.snackBar.open('Pagamento effettuato', 'Chiudi', {
+            duration: 5000,
+          });
+          //window.location.reload();
+        });
+      } catch (error: any) {
         this.isProgressSpinnerVisible = false;
-        //window.location.reload();
-      });
+        this.snackBar.open(error.message, 'Chiudi', {
+          duration: 5000,
+        });
+      }
     });
   }
 
   async getToken() {
-    await this.web3.pullTCoin();
+    try {
+      this.isProgressSpinnerVisible = true;
+      await this.web3.pullTCoin().finally(() => {
+        this.snackBar.open('Token ricevuti! Have fun!', 'Chiudi', {
+          duration: 5000,
+        });
+      });
+    } catch (error: any) {
+      this.snackBar.open(error.message, 'Chiudi', {
+        duration: 5000,
+      });
+    }
+    this.isProgressSpinnerVisible = false;
   }
 
   async getTokenBalance() {
