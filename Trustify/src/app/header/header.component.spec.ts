a9db52e919e929072d49fda79ce43754e9778ac6
Luca Marcato <marcatoluca15@gmail.com>
2023-05-06 11:25:22 +0200

    aggiunti test per il componente header

--- Trustify/src/app/header/header.component.spec.ts
+++ Trustify/src/app/header/header.component.spec.ts
@@ -2,14 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HeaderComponent } from './header.component';
 import { WalletService } from '../wallet.service';
+import { By } from '@angular/platform-browser';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let walletServiceSpy: any;
 
   beforeEach(async () => {
 
-    let walletServiceSpy = jasmine.createSpyObj('WalletService', ['connect'])
+    walletServiceSpy = jasmine.createSpyObj('WalletService', ['connect'])
 
     await TestBed.configureTestingModule({
       declarations: [HeaderComponent],
@@ -20,10 +22,49 @@ describe('HeaderComponent', () => {
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should not detect Metamask connection state on component initialize', () => {
+    component.ngOnInit();
+    fixture.detectChanges();
+    expect(walletServiceSpy.connect).not.toHaveBeenCalled();
+  })
+
+  it('should detect Metamask connection state on component reload', () => {
+    spyOn(window.localStorage, 'getItem').and.returnValue('true');
+    component.ngOnInit();
+    fixture.detectChanges();
+    expect(walletServiceSpy.connect).toHaveBeenCalled();
+  })
+
+  it('should call changeMetamaskState() on button click', () => {
+    const metamaskFunction = spyOn(component, 'changeMetamaskState');
+    component.changeMetamaskState();
+
+    expect(metamaskFunction).toHaveBeenCalled();
+  })
+
+  it('should show pagamento and areaPersonale pages on Metamask connected', () => {
+    component.isMetamaskConnected = true;
+    fixture.detectChanges();
+
+    const pagamentoTab = fixture.debugElement.query(By.css('#pagamento'));
+    const areaPersonaleTab = fixture.debugElement.query(By.css('#area-personale'));
+
+    expect(pagamentoTab).toBeTruthy();
+    expect(pagamentoTab.nativeElement.getAttribute('label')).toEqual('Effettua un pagamento');
+    expect(areaPersonaleTab).toBeTruthy();
+    expect(areaPersonaleTab.nativeElement.getAttribute('label')).toEqual('Area personale');
+  })
+
+  it('should not show pagamento and areaPersonale pages on Metamask disconnected', () => {
+    component.isMetamaskConnected = false;
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('#pagamento'))).toBeNull();
+    expect(fixture.debugElement.query(By.css('#area-personale'))).toBeNull();
+  })
 });
