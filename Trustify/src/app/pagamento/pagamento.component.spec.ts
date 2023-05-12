923b2cc8b94ca71edb22ed9324146387f1e3d5b7
Luca Marcato <marcatoluca15@gmail.com>
2023-05-12 15:49:35 +0200

    Tracciati test RFO7 componente pagamento

--- Trustify/src/app/pagamento/pagamento.component.spec.ts
+++ Trustify/src/app/pagamento/pagamento.component.spec.ts
@@ -17,6 +17,8 @@ describe('PagamentoComponent', () => {
   let component: PagamentoComponent;
   let fixture: ComponentFixture<PagamentoComponent>;
   let web3ServiceSpy: any;
+  let approveTokensSpy: any;
+  let depositTokensSpy: any;
 
   beforeEach(async () => {
     web3ServiceSpy = jasmine.createSpyObj('Web3Service', [
@@ -25,6 +27,9 @@ describe('PagamentoComponent', () => {
       'pullTCoin',
       'getTokenBalance',
     ]);
+
+    approveTokensSpy = web3ServiceSpy.approveTokens.and.returnValue(Promise.resolve(true));
+    depositTokensSpy = web3ServiceSpy.depositTokens.and.returnValue(Promise.resolve(true));
 
     await TestBed.configureTestingModule({
       declarations: [PagamentoComponent],
@@ -40,6 +45,59 @@ describe('PagamentoComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('RFO7 - user should be able to make a payment from his wallet calling component pay', () => {
+    fixture.detectChanges();
+    const paySpy = spyOn(component, 'pay');
+    const addressControl = component.form.controls['address'];
+    const tokenControl = component.form.controls['tokens'];
+    addressControl.setValue('0x96A85348123DfAc720fFa6193dE5c9792BB65C5e');
+    tokenControl.setValue('10');
+
+    fixture.detectChanges();
+
+    const submitButton = fixture.debugElement.query(By.css('#btn-submit'));
+    (submitButton.nativeElement as HTMLButtonElement).click();
+
+    expect(paySpy).toHaveBeenCalled();
+  });
+
+  it('RFO7 - user should be able to make a payment from his wallet calling web3 approveTokens and depositTokens', fakeAsync(() => {
+    component.pay('0x96A85348123DfAc720fFa6193dE5c9792BB65C5e', '10');
+
+    tick();
+
+    fixture.detectChanges();
+    expect(approveTokensSpy).toHaveBeenCalled();
+  }));
+
+  it('RFO7.1 - user should be able to see the error message if the connection is lost', () => {
+
+  });
+
+  it('RFO7.2 - user should be able to enter the address of the wallet intended to receive the payment', () => {
+
+  });
+
+  it('RFO7.2.1 - user should be able to see the error message if the address is empty', () => {
+
+  });
+
+  it('RFO7.2.2 - user should be able to see the error message if the address is invalid', () => {
+
+  });
+
+  it('RFO7.3 - user should be able to enter the amount of ERC20 token to send', () => {
+
+  });
+
+  it('RFO7.3.1 - user should be able to see the error message is the token amount is empty', () => {
+
+  })
+
+  it('RFO7.3.2 - user should be able to see the error message is the token amount is invalid', () => {
+
+  })
 
   it('should call component getTokenBalance() on component initialize', () => {
     spyOn(component, 'getTokenBalance');
@@ -60,20 +118,5 @@ describe('PagamentoComponent', () => {
     expect(getTokenBalanceSpy).toHaveBeenCalled();
     expect(getTokenBalanceSpy.calls.any()).toBe(true);
     expect(component.balance).toBeDefined();
-  }));
-
-  it('should call component pay() after form submit', waitForAsync(() => {
-    const addressControl = component.form.get('address');
-    const tokenControl = component.form.get('token');
-    const formElement = fixture.debugElement.query(By.css('#payment'));
-    const submitFunction = spyOn(component, 'pay');
-
-    addressControl?.setValue('0x96A85348653DfAd720fFa6193dE5c9792BB65C5e');
-    tokenControl?.setValue(10);
-    fixture.detectChanges();
-    formElement.triggerEventHandler('submit', {});
-    fixture.detectChanges();
-
-    expect(submitFunction).toHaveBeenCalled();
   }));
 });
