1963af49cc589fa221e1b425b02f60a3fcf28b1b
lazza <nicola.lazzarin89@gmail.com>
2023-05-20 11:11:08 +0200

    added test for wallet service

--- Trustify/src/app/wallet.service.spec.ts
+++ Trustify/src/app/wallet.service.spec.ts
@@ -38,6 +38,62 @@ describe('WalletService', () => {
       'https://sepolia.infura.io/v3/1caadfe504ce4531b041de4bc8927ceb'
     );
   });
+
+  it('should return a valid provider', () => {
+    expect(service.getProvider()).toEqual(
+      'https://sepolia.infura.io/v3/1caadfe504ce4531b041de4bc8927ceb'
+    );
+  });
+
+  it('should return an valid address', () => {
+    const getAccountSpy = spyOn(service, 'getAccount').and.returnValue(
+      Promise.resolve('0x96A85348123DfAc720fFa6193dE5c9792BB65C5e')
+    );
+    service.getAccount().then((response) => {
+      expect(response).toEqual('0x96A85348123DfAc720fFa6193dE5c9792BB65C5e');
+    });
+    expect(getAccountSpy).toHaveBeenCalled();
+  });
+
+  it('should return true if metamask is installed', () => {
+    const isInstalledSpy = spyOn(service, 'isInstalled').and.returnValue(true);
+
+    expect(service.isInstalled()).toBeTrue();
+    expect(isInstalledSpy).toHaveBeenCalled();
+  });
+
+  it('should return false if metamask is not installed', () => {
+    const isInstalledSpy = spyOn(service, 'isInstalled').and.returnValue(false);
+
+    expect(service.isInstalled()).toBeFalse();
+    expect(isInstalledSpy).toHaveBeenCalled();
+  });
+
+  it('should return true if wallet is connected', () => {
+    const isWalletConnectedSpy = spyOn(
+      service,
+      'isWalletConnected'
+    ).and.returnValue(Promise.resolve(true));
+
+    service.isWalletConnected().then((response) => {
+      expect(response).toBeTrue;
+    });
+
+    expect(isWalletConnectedSpy).toHaveBeenCalled();
+  });
+
+  it('should return false if wallet is disconnected', () => {
+    const isWalletConnectedSpy = spyOn(
+      service,
+      'isWalletConnected'
+    ).and.returnValue(Promise.resolve(false));
+
+    service.isWalletConnected().then((response) => {
+      expect(response).toBeFalse;
+    });
+
+    expect(isWalletConnectedSpy).toHaveBeenCalled();
+  });
 });
 
 describe('WalletService', () => {
